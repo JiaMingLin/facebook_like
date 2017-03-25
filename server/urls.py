03126c4fe92a7f05ba183748e3ad6c29411728f9
JiaMingLin <robin890650@gmail.com>
2017-03-25 18:12:04 +0800

    add view import

--- server/urls.py
+++ server/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+import views
 
 urlpatterns = [
 	url(r'^fb_like/', include('fb_like.urls')),
